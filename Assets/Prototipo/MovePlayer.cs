f993d59396b3914ca9951be12ebf598b395b9023
Kazin <kaiocostasilva084@gmail.com>
2024-08-12 10:31:47 -0300

    envio-prototipo1

---
+++ Assets/Prototipo/MovePlayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
